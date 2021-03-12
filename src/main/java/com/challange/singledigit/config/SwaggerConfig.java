ecd449e79efaa1e8a0d22f2695eb859b9b5397e9
Rodrigo França <rodrigo_rjsf@hotmail.com>
2021-03-12 20:42:07 -0300

    Gitlab CI e Heroku java configuration.

--- src/main/java/com/challange/singledigit/config/SwaggerConfig.java
+++ src/main/java/com/challange/singledigit/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.challange.singledigit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,12 +10,12 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
 
     @Bean
     public Docket api() {
